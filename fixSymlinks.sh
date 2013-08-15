9b52b07fdf91f5ec20401dc5d06c8b6fe3f05e8e
Tim Franssen <tim.franssen@nedap.com>
2013-08-15 13:28:36 +0200

    Added coffeescript support for vim, made symlink scripts, some minor tweaks :)

---
+++ fixSymlinks.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+ln -s ~/dotfiles/dotvim ~/.vim
+ln -s ~/dotfiles/dotbash_profile ~/.bash_profile
+ln -s ~/dotfiles/dotvimrc ~/.vimrc
+ln -s ~/dotfiles/dottmux.conf ~/.tmux.conf
---
+++ removeOldFiles.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+rm -r ~/.vim
+rm ~/.bash_profile
+rm ~/.vimrc
+rm ~/.tmux.conf
